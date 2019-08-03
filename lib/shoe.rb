393120c88b028e633e8d8c622a9b601d789fc3b1
Randy Taylor <randyisbusy@gmail.com>
2019-08-03 19:02:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,7 @@ class Shoe
     @brand = brand
   end
   attr_accessor :brand, :color, :size, :material, :condition
-  def cobble 
+  def cobble
     puts "Your shoe is as good as new!"
     :condition = "new"
   end
